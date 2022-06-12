803828be31268e40d2c6005bd920b1bca9e2fbec
Guilherme cavazzini <guilhermescavazzini@outlook.com>
2022-06-12 15:46:57 -0400

    adicionando o exercicio 01

---
+++ exercicios/L03_EX01.py
@@ -0,0 +1,14 @@
+aprovados = []
+notas = 0
+alunos = 1
+
+for h in range(1):
+    for z in range(10):
+        print('Insira a nota do', alunos, '° aluno')
+        notas = int(input('insira a 1° nota: ')) + int(input('insira a 2° nota: ')) + int(
+            input('insira a 3° nota: ')) + int(input("insira a 4° nota: "))
+        if notas / 4 >= 7:
+            aprovados.append(+1)
+            alunos = alunos + 1
+
+print(len(aprovados), 'alunos foram aprovados')
