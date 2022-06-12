fa4156eeb134a26487496085c00a8ee46d26cd31
Guilherme cavazzini <guilhermescavazzini@outlook.com>
2022-06-12 17:40:52 -0400

    "exercicio completo"

--- exercicios/L03_EX02.py
+++ exercicios/L03_EX02.py
@@ -1,2 +1,18 @@
-print('ola')
-print("gg")
+vetor = []
+x = 1
+multiplicacao = 1
+soma = 0
+
+for x in range(5):
+    vetor.append(int(input('insira os numeros: ')))
+
+for v in vetor:
+    soma += v
+    multiplicacao *= v
+
+print('-__-'*32)
+print(vetor)
+print('-__-'*32)
+print('a soma desses numeros são:', soma)
+print('-__-'*32)
+print('a multiplicação desses numeros é:', multiplicacao)
